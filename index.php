4be8ec432de2f3cf8aa69929b02e90c57b928d0b
John Michael Camara <johnmichael.camara.git@gmail.com>
2024-07-07 03:26:50 +0800

    First Commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Library</title>
+    <link rel="stylesheet" src="./bootstrap-5.3.3-dist/css/bootstrap.min.css">
+</head>
+
+<body>
+
+</body>
+<script src="./bootstrap-5.3.3-dist/js/bootstrap.min.js"></script>
+<script src="./jquery/jquery-3.7.1.min.js"></script>
+
+</html>
