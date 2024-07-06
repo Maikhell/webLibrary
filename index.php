80c70d85b37970b5bbaa7a8fa5c41997b3961aa2
John Michael Camara <johnmichael.camara.git@gmail.com>
2024-07-07 03:53:41 +0800

    First Commit

--- index.php
+++ index.php
@@ -5,11 +5,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Library</title>
-    <link rel="stylesheet" src="./bootstrap-5.3.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./bootstrap-5.3.3-dist/css/bootstrap.min.css">
 </head>
 
 <body>
-
+    <nav class="navbar bg-body-tertiary">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="./images/logo.png" alt="Bootstrap" width="45" height="45">
+            </a>
+        </div>
+    </nav>
 </body>
 <script src="./bootstrap-5.3.3-dist/js/bootstrap.min.js"></script>
 <script src="./jquery/jquery-3.7.1.min.js"></script>
