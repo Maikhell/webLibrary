8e45959f979a119ba9442dcc97fce63ead23dbcb
John Michael Camara <johnmichael.camara.git@gmail.com>
2024-09-01 00:11:14 +0800

    Minor Changes

--- index.php
+++ index.php
@@ -5,14 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Library</title>
-    <link rel="stylesheet" href="./design/base.css">
+    <link rel="stylesheet" href="./design/main.css">
     <link rel="stylesheet" href="./bootstrap-5.3.3-dist/css/bootstrap.min.css">
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" id="myNavbar">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="#">MaiAnime</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -56,55 +56,29 @@
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
             </div>
-            <div class="carousel-inner">
+            <div class="carousel-inner bg-dark text-white">
                 <!-- Slide 1 -->
                 <div class="carousel-item active">
-                    <div class="d-flex">
-                        <div class="info-section p-3">
-                            <h3>Information fora Slide 1</h3>
-                            <p>This is some info for the first slide.</p>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm-3 ">
+                                <div class="info-section p-3">
+                                    <h3>Bleach</h3>
+                                    <p>This is some info for the first slide.</p>
+                                </div class="col-sm-5 ">
+                                <img src="./images/FOREST-NIGHT 8k.png" class=" d-block w-100" alt="...">
+                            </div>
                         </div>
-                        <img src="./images/bleach.jpg" class="d-block w-25" alt="...">
-                    </div>
-                </div>
-
-                <!-- Slide 2 -->
-                <div class="carousel-item">
-                    <div class="d-flex">
-                        <div class="info-section p-3">
-                            <h3>Information for Slide 2</h3>
-                            <p>This is some info for the second slide.</p>
-                        </div>
-                        <img src="..." class="d-block w-100" alt="...">
-                    </div>
-                </div>
-
-                <!-- Slide 3 -->
-                <div class="carousel-item">
-                    <div class="d-flex">
-                        <div class="info-section p-3">
-                            <h3>Information for Slide 3</h3>
-                            <p>This is some info for the third slide.</p>
-                        </div>
-                        <img src="..." class="d-block w-100" alt="...">
                     </div>
                 </div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
         </div>
     </div>
 
     <div class="container" id="card-layout">
         <div class="row">
             <div class="col-sm-3">
-                <div class="card" style="width: 18rem;">
+                <div class="card" style="width: 14rem;">
                     <img src="./images/bleach.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
 
@@ -115,10 +89,7 @@
                     </div>
                 </div>
             </div>
-
-
         </div>
-
     </div>
 </body>
 <script src="./bootstrap-5.3.3-dist/js/bootstrap.min.js"></script>
