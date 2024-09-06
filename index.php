043e3582c49c30a2245c97c816bc125a1740f7f0
John Michael Camara <johnmichael.camara.git@gmail.com>
2024-09-06 23:48:49 +0800

    Minor changes

--- index.php
+++ index.php
@@ -5,14 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Library</title>
-    <link rel="stylesheet" href="./design/main.css">
+    <link rel="stylesheet" href="./design/mains.css">
     <link rel="stylesheet" href="./bootstrap-5.3.3-dist/css/bootstrap.min.css">
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark" id="myNavbar">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">MaiAnime</a>
+            <a class="navbar-brand" href="#">Re: Libraries</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -61,14 +61,57 @@
                 <div class="carousel-item active">
                     <div class="d-flex justify-content-between">
                         <div class="info-section p-3">
-                            <h3>Bleach</h3>
-                            <p>This is some info for the first slide.</p>
+                            <h1>Top Book 1 </h1>
+                            <h5>Author Name </h5>
+                            <p>Date Published </p>
+                            <span class="badge bg-light text-dark">Classic </span>
+                            <span class="badge bg-light text-dark">Drama </span>
+                            <span class="badge bg-light text-dark">Crime </span>
+                            <span class="badge bg-light text-dark">Anthology</span>
+
+                            <p class="info-carousel">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi facilis accusamus assumenda pariatur, quis quasi, tempore ea expedita ducimus omnis sapiente illum perspiciatis rerum fugiat fuga aliquid quisquam delectus vero.
+                            </p>
+
+                            <p>Language Available</p>
+                            <span class="badge bg-info text-dark">En</span>
+                            <span class="badge bg-warning text-dark">Es</span>
+                            <span class="badge bg-success text-dark">Fr</span>
+                            <span class="badge bg-danger text-dark">Jp</span>
+                        </div>
+                        <img src="./images/FOREST-NIGHT 8k.png" class=" d-block w-50" alt="...">
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="d-flex justify-content-between">
+                        <div class="info-section p-3">
+                            <h3>Top Book 2 </h3>
+                            <p>Info about the book</p>
+                        </div>
+                        <img src="./images/FOREST-NIGHT 8k.png" class=" d-block w-50" alt="...">
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="d-flex justify-content-between">
+                        <div class="info-section p-3">
+                            <h3>Top Book 3 </h3>
+                            <p>Info about the book</p>
                         </div>
                         <img src="./images/FOREST-NIGHT 8k.png" class=" d-block w-50" alt="...">
                     </div>
                 </div>
             </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
         </div>
+    </div>
+
+    </div>
     </div>
 
     <div class="container" id="card-layout">
@@ -78,10 +121,10 @@
                     <img src="./images/bleach.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
 
-                        <h5 class="card-title">Bleach</h5>
-                        <span class="badge bg-info text-dark"> Sub-366Ep</span>
+                        <h5 class="card-title">Book 1 </h5>
+                        <span class="badge bg-info text-dark">En</span>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Watch Now</a>
+                        <a href="#" class="btn btn-primary">Read Now</a>
                     </div>
                 </div>
             </div>
